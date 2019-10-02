b083b827a1a40ae12272b825deff334c98cb25c8
khenisiiraoudha123 <khenissiraoudha16@gmail.com>
2019-10-02 17:47:47 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Time from './time'
+function App() {
+  return (
+
+    <div>
+
+      <Time ms={6578934}/>
+      
+    </div>
+    
+  );
+}
+
+export default App;
